de5d16a65ee800fc6e5a9af24461706514d20998
KCSITGLOBAL\ashish.mule <ashish.mule@kcsitglobal.com>
2022-06-07 17:16:48 +0530

    modified for production

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tools-utilities/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
