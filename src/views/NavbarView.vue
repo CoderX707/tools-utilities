8da20f481e6903de9a9b14309cec0ecd5a857f56
Ashish <ashishmule946@gmail.com>
2022-11-19 16:06:35 +0530

    minor color issue

--- src/views/NavbarView.vue
+++ src/views/NavbarView.vue
@@ -147,7 +147,7 @@ export default {
             <svg
               v-else
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              class="h-6 w-6 text-black"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
